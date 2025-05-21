c5f767bdca4a8fe697e215863f3bab8d293c0493
Manojxetri <khanalmanoj562@gmail.com>
2025-05-21 15:28:12 +0545

    third commit

---
+++ banking/script.js
@@ -0,0 +1,80 @@
+// Display a menu using a switch statement:
+
+// 1: Check Balance
+
+// 2: Deposit Money
+
+// 3: Withdraw Money
+
+// 4: Exit
+
+// Use a do-while loop to repeat the menu until the user selects exit.
+
+// Use a for loop to simulate transaction history print last 5 transactions as dummy data.
+
+
+
+let balance = 40000;
+let menu;
+
+do {
+  menu = parseInt(prompt(`Press:
+  1: Check Balance
+  2: Deposit Money
+  3: Withdraw Money
+  4: Exit`));
+
+  switch(menu){
+    case 1:
+      console.log("Total Balance:", balance);
+      break;
+
+    case 2:
+      let depositMoney = parseInt(prompt("Enter the amount to deposit:"));
+      if ( depositMoney > 0) {
+        balance =balance + depositMoney;
+        console.log("Deposit successful. Total Balance:", balance);
+      } else {
+        console.log("Enter amount greater than 0");
+      }
+      break;
+
+    case 3:
+      let withdrawMoney = parseInt(prompt("Enter the amount to withdraw:"));
+      if (withdrawMoney > 0) {
+        if (withdrawMoney <= balance) {
+          balance = balance - withdrawMoney;
+          console.log("Withdrawal successful. Total Balance:", balance);
+        } else {
+          console.log("Insufficient balance.");
+        }
+      } else {
+        console.log("Enter amount greater than 0");
+      }
+      break;
+
+    case 4:
+      console.log("You have exited the system.");
+      break;
+
+    default:
+      console.log("Please enter a valid option (1–4).");
+  }
+
+} while(menu !== 4);
+
+
+//transaction history using dummy data and for loop
+
+let transactionHistory = [
+  "Deposited Rs. 5000",
+  "Withdrew Rs. 2000",
+  "Deposited Rs. 3000",
+  "Withdrew Rs. 1000",
+  "Deposited Rs. 8000"
+];
+
+console.log("Last 5 Transactions");
+for (let i = 0; i < transactionHistory.length; i++) {
+  console.log((i + 1) + ". " + transactionHistory[i]);
+}
